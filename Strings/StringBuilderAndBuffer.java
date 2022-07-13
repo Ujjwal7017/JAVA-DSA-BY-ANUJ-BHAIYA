3a299897406f8479a00b3d98c7c13637b52d1b24
Ujjwal Shukla <73957886+Ujjwal7017@users.noreply.github.com>
2022-07-13 17:54:41 +0530

    Create StringBuilderAndBuffer.java

---
+++ Strings/StringBuilderAndBuffer.java
@@ -0,0 +1,19 @@
+public class StringBuilderAndBuffer {
+
+	public static void main(String[] args) {
+		
+		String a = "abc";
+		a.concat(" HI");
+		
+		StringBuilder b = new StringBuilder("Hello");
+		
+		b.append(" HI");
+		b.replace(0, 3, "CATSPEED");
+		b.reverse();
+		
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
