887a444db2c98714cde98630aec6d1269ce02f9a
Ujjwal Shukla <73957886+Ujjwal7017@users.noreply.github.com>
2022-08-02 23:47:56 +0530

    Create Learn_Sets.java

---
+++ Java_Collection_Frameworks/Learn_Sets.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class LearnSets {
+
+	public static void main(String[] args) {
+		
+		Set<String> set = new HashSet<>();
+		set.add("Kiwi");
+		set.add("Apple");
+		set.add("Mango");
+		set.add("Orange");
+		
+		set.remove("Kiwi");
+		
+		System.out.println(set);
+
+	}
+
+}
